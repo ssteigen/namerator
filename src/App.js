246e56c630695e88745de0727418ccf753c14058
Savanah Steigen <steigen@theinstitutes.org>
2020-04-06 13:29:50 -0400

    Create basic app structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+function Layout({children}) {
+  return (
+    <div className="h-full w-full flex flex-auto flex-col justify-center align-middle">
+      {children}
+    </div>
+  );
+}
+
+function Header({children}) {
+  return (
+    <h1 className="mb-4 text-center text-3xl font-bold leading-7 text-gray-900">
+      {children}
+    </h1>
+  );
+}
+
+function GeneratedName() {
+  return (
+    <span className="generated-name">Thunder Ponies</span>
+  );
+}
+
+function Button({children, color="indigo", className="", onClick, ...rest}) {
+  return (
+    <button onClick={onClick} className={`p-2 rounded-md text-white bg-${color}-600 hover:bg-${color}-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out ${className}`} {...rest}>
+      {children}
+    </button>
+  );
+}
+
+function generateName() {
+  alert ("hello");
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header>Namerator</Header>
+      <div className="text-center">
+        <GeneratedName></GeneratedName>
+        <Button onClick={generateName} className="ml-2">Generate</Button>
+      </div>
+    </Layout>
   );
 }
 
